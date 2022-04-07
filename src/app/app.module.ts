51a45ffc180f2a415e167eebaadf21c3acada442
Nova Boman <nova.boman@chasacademy.se>
2022-04-07 20:17:22 +0200

    Create login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppComponent } from './app.component';
 import { PublicModule } from './public/public.module';
 import { SecureComponent } from './secure/secure.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    SecureComponent
+    SecureComponent,
+
   ],
   imports: [
     BrowserModule,
--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { PublicComponent } from './public.component';
 import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
     PublicComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
