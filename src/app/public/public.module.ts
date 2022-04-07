f1e285a3fea93e2fe6a99cf2166ba2d93055e0ed
Nova Boman <nova.boman@chasacademy.se>
2022-04-07 20:05:03 +0200

    Import RouterModule

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicComponent } from './public.component';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -11,7 +12,8 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class PublicModule { }
