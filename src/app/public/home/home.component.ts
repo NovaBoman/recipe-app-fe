c22ec8f361a89ffba30f501456e3c5099b917d95
Nova Boman <nova.boman@chasacademy.se>
2022-04-13 18:09:05 +0200

    Style components

--- src/app/public/home/home.component.ts
+++ src/app/public/home/home.component.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
   recipes: any;
-  constructor(private recipeService: RecipeService) { }
+  user: any;
+  constructor(private recipeService: RecipeService) {}
 
   ngOnInit(): void {
     this.recipeService.getRandomRecipes(2).subscribe({
-      next: (result: any) => this.recipes = result.recipes
+      next: (result: any) => (this.recipes = result.recipes),
     });
+
+    this.loggedIn();
   }
 
+  loggedIn() {
+    if (localStorage.getItem('user') !== null) {
+      this.user = true;
+    } else {
+      this.user = false;
+    }
+    return this.user;
+  }
 }
