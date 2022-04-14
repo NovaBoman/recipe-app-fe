03d07b7a044a58949b8be69634fb2c39f4066775
Nova Boman <nova.boman@chasacademy.se>
2022-04-14 13:00:25 +0200

    Set environment variable for development

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_KEY: 'apiKey=8dcc09e4297c48a9821e1d4192a9bb19',
 };
 
 /*
